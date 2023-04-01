3b1e04a1cd8d1b3c559fc4f52d5532442658f9ba
MarcoTheBigCreator <marco.rodriguez.is@unipolidgo.edu.mx>
2023-03-31 21:59:08 -0600

    ADD: refactor

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nltk
+import os
 
 # Define the grammar rules
 grammar1 = nltk.CFG.fromstring("""
@@ -15,7 +16,7 @@ E -> I
 E -> E '+' E
 E -> E '*' E
 E -> '(' E ')'
-I ->  epsilon | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
+I ->  ' ' | '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9'
 """)
 
 grammar3 = nltk.CFG.fromstring("""
@@ -26,39 +27,51 @@ C -> 'a' C 'd' | 'a' D 'd'
 D -> 'b' D 'c' | 'b' 'c'
 """)
 
+
 def grammars(grammarChosen, sentence):
     # Create a parser using the grammar
-    parser = nltk.ChartParser(grammarChosen)
+    parser = nltk.EarleyChartParser(grammarChosen)
 
     # Parse the sentence using the grammar
     try:
-        trees = parser.parse(sentence.split())
-        print("Sentence is grammatically correct!")
-        for tree in trees:
-            print(tree)
+        chart = parser.chart_parse(sentence)
+        if any(chart.parses(grammarChosen.start())):
+            print(f'{sentence} is a valid word in the grammar.')
+        else:
+            print(f'{sentence} is NOT a valid word in the grammar!')
     except ValueError:
-        print("Sentence is not grammatically correct.")
-    main()
+        print(f'{sentence} is NOT a valid word in the grammar!')
+
+    answer = input("Do you want to try another sentence? (y/n): ")
+    if answer == 'y':
+        os.system('cls')
+        main()
+    elif answer == 'n':
+        exit()
+    else:
+        print("Invalid answer.")
 
 
 def main():
-    grammar = int(input("Enter the number of the grammar you want to use (1, 2, 3, 4(exit)): "))
+    grammar = int(input("Enter the number of the grammar you want to use:\n1: Grammar 1\n2: Grammar 2\n3: Grammar "
+                        "3\n4: Exit\nOption: "))
     if grammar == 1:
         # Define the input sentence to be parsed
-        sentence = input("Enter a sentence (separate the characters with a space): ")
+        sentence = input("Enter a sentence: ")
         grammars(grammar1, sentence)
     elif grammar == 2:
         # Define the input sentence to be parsed
-        sentence = input("Enter a sentence (separate the characters with a space): ")
+        sentence = input("Enter a sentence (use space for empty value): ")
         grammars(grammar2, sentence)
     elif grammar == 3:
         # Define the input sentence to be parsed
-        sentence = input("Enter a sentence (separate the characters with a space): ")
+        sentence = input("Enter a sentence: ")
         grammars(grammar3, sentence)
     elif grammar == 4:
-        exit
+        exit()
     else:
         print("Invalid grammar number.")
         main()
+
 
 main()
